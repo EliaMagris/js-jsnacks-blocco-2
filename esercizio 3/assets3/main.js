4728ab19ecac2ab02b4b10e56d471b06eec1f1a4
EliaMagris <elia.magris@libero.it>
2022-10-25 16:12:38 +0200

    3° esercizio completato

---
+++ esercizio 3/assets3/main.js
@@ -0,0 +1,11 @@
+let array = [1,2,3,4,5,6,7,8,9,10];
+
+let somma = 0;
+
+for(let i = 1; i < array.length; i +=2){
+     
+    somma += array[i];
+    console.log(somma)
+}
+
+// somma dei numeri in posizione dispari
