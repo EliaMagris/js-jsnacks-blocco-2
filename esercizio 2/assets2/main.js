2ce4b47099433daef5efe44c18175a38275d64cf
EliaMagris <elia.magris@libero.it>
2022-10-26 11:01:54 +0200

    4° esercizio completato

--- esercizio 2/assets2/main.js
+++ esercizio 2/assets2/main.js
@@ -7,8 +7,8 @@
 
 
 
- let randomnomi = nomi[Math.floor(Math.random(0) * countName)];
- let randomcognomi = cognomi[Math.floor(Math.random(0) * countCognomi)] ;
+  let randomnomi = nomi[Math.floor(Math.random(0) * countName)]; //scrivo la classe array per far si che il math random venga applicato ad essa
+  let randomcognomi = cognomi[Math.floor(Math.random(0) * countCognomi)] ;
  
  
  console.log(randomnomi + ' ' + randomcognomi);
--- esercizio 4/assets4/main.js
+++ esercizio 4/assets4/main.js
@@ -3,6 +3,27 @@ let array1 = [1,2,3,4,5,6,7,8,9,10]
 
 let array2 = [1,2,3,4,5]
 
-let aggiunta = array2.push(6,7,8,9,10)
+let arrayMaggiore;
+let arrayMinore;
 
-console.log(array2)
+if(array1.length > array2.length){
+  arrayMaggiore = array1;
+  arrayMinore = array2;
+
+}else if(array1.length = array2.length){
+    console.log("hanno lo stesso numero di dati")
+
+}else{
+    arrayMaggiore = array2;
+    arrayMinore = array1;
+}
+
+let differenzaLunghezzaArray = arrayMaggiore.length - arrayMinore.length;
+
+for(let i = 0; i < differenzaLunghezzaArray; i++){
+    let numeroRandom = Math.floor(Math.random()* 100);
+    
+    arrayMinore.push(numeroRandom);
+}
+
+console.log(arrayMinore);
